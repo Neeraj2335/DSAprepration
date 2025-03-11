9f16930ccd89a99c951da600abecbdb283a4e6bb
Neeraj yadav <neeraj.22scse1410025@galgotiasuniversity.edu.in>
2025-03-11 17:32:40 +0530

    SwapingWithTemp.java

---
+++ Basic/SwapingWithTemp.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("before swapping");
+          System.out.println(a);
+            System.out.println(b);
+              System.out.println("After swapping");
+              int c=a;
+              a=b;
+              b=c; 
+              System.out.println(a);
+              System.out.println(b);
+             
+               
+    }
+}
